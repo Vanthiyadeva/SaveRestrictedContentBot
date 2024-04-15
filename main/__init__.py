946081023e5d4462ce2afb6ab8205e4222eb11de
Vanthiyadeva <113768780+Vanthiyadeva@users.noreply.github.com>
2024-04-15 05:50:13 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 18717081
+API_HASH = "cbafb107aea26ae983f0924b4739a20e"
+BOT_TOKEN = "7064787118:AAFTVpfvGzq7OmTAijttZ8ESFMcKCqioZys"
+SESSION = "BQEdmZkAwCri8oTC9a8lxc1kePR_iHKyJP7tbPIBmMjqz3E6NsD2K5cRjdgkC_fNugg-U99fAIJWVU2pndHU0RsDZo35g0xzgLwQakA8aELO5uRz6Z2IOhVkQLehX_RLhDGTK7n8APsKrtyaWXCMd5KP002a96LxHo5eoJRUyewQqfS6x-SHOPlXt_DYVSBm4CKlx5gmu6IxFUDJ65Dp2QHGE55iRs1cqfQNfXZ27eJvjDLtUexx3FeSpa-ZT_cC39_Avn0tQIDav6xq7h-F2Xjt_rOfuAAJySSwsVLKvkjQ2hATBW5pXhtOE3e7KI_2RET2RAC0yNx_41i0VqV342wznrfnqQAAAAFF4RiQAA"
+FORCESUB = "fmwlproof"
+AUTH = 5467347088
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
